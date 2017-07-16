cefffbb957e05b6c0830dd7b648d75ba35493677
ryugohan <ryugohan@gmail.com>
2017-07-16 20:38:37 +0100

    Initial commit

---
+++ app/src/main/java/com/adam/movielist/SettingsActivity.java
@@ -0,0 +1,45 @@
+package com.adam.movielist;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        bindPreferenceSummaryToValue(findPreference("sortby"));
+    }
+
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String stringValue = newValue.toString();
+        if(preference instanceof ListPreference)
+        {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+
+            if(prefIndex >=0)
+            {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }
+        else
+        {
+            preference.setSummary(stringValue);
+        }
+        return false;
+    }
+    private void  bindPreferenceSummaryToValue(Preference preference)
+    {
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference, PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(),""));
+    }
+}
